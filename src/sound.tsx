1c05f20c0534db3a0c162c5f1da5ab264fb4f55a
star14ms <danalstj@naver.com>
2022-07-04 17:49:06 +0900

    Code Cleanup

--- src/sound.tsx
+++ src/sound.tsx
@@ -1,40 +1,35 @@
 import { Audio } from "expo-av";
-import { throwIfAudioIsDisabled } from "expo-av/build/Audio/AudioAvailability";
 
 
 export default class Sounds {
-    sounds: {[key: string]: {object: Audio.Sound, path: string, isPlaying: boolean}};
+    sounds: {[key: string]: Audio.Sound};
   
     constructor() {
       this.sounds = {}
       this.load_sounds()
     }
 
-    register(soundName: string, path: string) {
-      this.sounds[soundName] = {
-        object: new Audio.Sound(),
-        path: path,
-        isPlaying: false,
-      }
+    register(soundName: string) {
+      this.sounds[soundName] = new Audio.Sound()
     }
 
     load_sounds = async () => {
-      this.register("피싱", "../assets/피싱.wav")
-      this.register("쉭", "../assets/쉭.wav")
-      this.register("undertale_change_color", "../assets/undertale_change_color.wav")
-      this.register("sans_voice", "../assets/sans_voice.wav")
-      this.register("바람", "../assets/[효과음]바람1.mp3")
-      this.register("먹기", "../assets/[효과음]먹기1.mp3")
+      this.register("피싱")
+      this.register("쉭")
+      this.register("undertale_change_color")
+      this.register("sans_voice")
+      this.register("바람")
+      this.register("먹기")
 
-      await this.sounds["피싱"].object.loadAsync(require("../assets/피싱.wav"));
-      await this.sounds["쉭"].object.loadAsync(require("../assets/쉭.wav"));
-      await this.sounds["undertale_change_color"].object.loadAsync(require("../assets/undertale_change_color.wav"));
-      await this.sounds["sans_voice"].object.loadAsync(require("../assets/sans_voice.wav"));
-      await this.sounds["바람"].object.loadAsync(require("../assets/[효과음]바람1.mp3"));
-      await this.sounds["먹기"].object.loadAsync(require("../assets/[효과음]먹기1.mp3"));
+      await this.sounds["피싱"].loadAsync(require("../assets/피싱.wav"));
+      await this.sounds["쉭"].loadAsync(require("../assets/쉭.wav"));
+      await this.sounds["undertale_change_color"].loadAsync(require("../assets/undertale_change_color.wav"));
+      await this.sounds["sans_voice"].loadAsync(require("../assets/sans_voice.wav"));
+      await this.sounds["바람"].loadAsync(require("../assets/[효과음]바람1.mp3"));
+      await this.sounds["먹기"].loadAsync(require("../assets/[효과음]먹기1.mp3"));
     }
 
     play = async (sound: string) => {
-      await this.sounds[sound].object.replayAsync();
+      await this.sounds[sound].replayAsync();
     };
 }
