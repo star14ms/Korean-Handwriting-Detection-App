a6afcad2e40142fd2b49ee6cb00f95c4209ffed5
star14ms <danalstj@naver.com>
2022-07-05 20:35:24 +0900

    Move Sound Files

--- src/sound.tsx
+++ src/sound.tsx
@@ -21,12 +21,12 @@ export default class Sounds {
       this.register("바람")
       this.register("먹기")
 
-      await this.sounds["피싱"].loadAsync(require("../assets/피싱.wav"));
-      await this.sounds["쉭"].loadAsync(require("../assets/쉭.wav"));
-      await this.sounds["undertale_change_color"].loadAsync(require("../assets/undertale_change_color.wav"));
-      await this.sounds["sans_voice"].loadAsync(require("../assets/sans_voice.wav"));
-      await this.sounds["바람"].loadAsync(require("../assets/[효과음]바람1.mp3"));
-      await this.sounds["먹기"].loadAsync(require("../assets/[효과음]먹기1.mp3"));
+      await this.sounds["피싱"].loadAsync(require("../assets/sounds/피싱.wav"));
+      await this.sounds["쉭"].loadAsync(require("../assets/sounds/쉭.wav"));
+      await this.sounds["undertale_change_color"].loadAsync(require("../assets/sounds/undertale_change_color.wav"));
+      await this.sounds["sans_voice"].loadAsync(require("../assets/sounds/sans_voice.wav"));
+      await this.sounds["바람"].loadAsync(require("../assets/sounds/[효과음]바람1.mp3"));
+      await this.sounds["먹기"].loadAsync(require("../assets/sounds/[효과음]먹기1.mp3"));
     }
 
     play = async (sound: string) => {
